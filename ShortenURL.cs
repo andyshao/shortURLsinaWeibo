8b25590fe564db7d7cefcc0d84352fd9fc0ac6ff
martianchm <martianchm@gmail.com>
2013-05-02 15:49:53 +0800

    Update ShortenURL.cs

--- ShortenURL.cs
+++ ShortenURL.cs
@@ -20,5 +20,17 @@ namespace EPMS.Web.Helper
            string[] strArray = HttpUtility.UrlDecode(client.DownloadData(address), Encoding.UTF8).Split(new string[] { "\"" }, StringSplitOptions.None);
            return  strArray[3];
        }
+       
+       public static  string ShortenV2(string url)
+       {
+           WebClient client = new WebClient();
+           WebHeaderCollection headers = new WebHeaderCollection();
+           headers[HttpRequestHeader.ContentType] = "application/x-www-form-urlencoded; charset=utf-8";
+           headers[HttpRequestHeader.Referer] = "http://api.t.sina.com.cn/";
+           client.Headers = headers;
+            string address = string.Format("https://api.weibo.com/2/short_url/shorten.json?access_token={0}&url_long={1}","2.00ttSGDCpeE7vB79abfd2b110ia1Ux", url);
+           string[] strArray = HttpUtility.UrlDecode(client.DownloadData(address), Encoding.UTF8).Split(new string[] { "\"" }, StringSplitOptions.None);
+           return  strArray[7];
+       }
     }
 }
